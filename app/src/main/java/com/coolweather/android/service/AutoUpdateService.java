d2544a0ec8aaeb660a6ed1ceff3c824fe2e5fefe
samuel-ai <samuel_ai@126.com>
2017-08-23 16:57:17 +0800

    修改程序图标和名称

--- app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
+++ app/src/main/java/com/coolweather/android/service/AutoUpdateService.java
@@ -41,8 +41,8 @@ public class AutoUpdateService extends Service {
         Log.d(TAG,"updateBingPic");
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         //这是8小时的毫秒数
-//        int anHour = 8 * 60 * 60 * 1000;
-        int anHour = 5000;
+        int anHour = 8 * 60 * 60 * 1000;
+//        int anHour = 5000;
         long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
         Intent i = new Intent(this, AutoUpdateService.class);
         PendingIntent pi = PendingIntent.getService(this,0,i,0);
